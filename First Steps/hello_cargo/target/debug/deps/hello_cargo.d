1b0db591614ead02d8cb4b0cf607020b492fb36b
Matthew M Lewis <echomatt95@gmail.com>
2020-05-06 00:14:54 -0500

    Chapter One

---
+++ First Steps/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\kmatt\Desktop\Rust\First Steps\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\kmatt\Desktop\Rust\First Steps\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
